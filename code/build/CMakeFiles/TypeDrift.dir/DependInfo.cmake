c37d15a04ea90509cd0d7aca4e376fe1b370a578
luimas834 <luimas834@gmail.com>
2026-03-27 23:48:44 +0600

    added few data

---
+++ code/build/CMakeFiles/TypeDrift.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/luimas834/Desktop/TypeDrift/code/src/auth/hashing.cpp" "CMakeFiles/TypeDrift.dir/src/auth/hashing.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/auth/hashing.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/auth/user_manager.cpp" "CMakeFiles/TypeDrift.dir/src/auth/user_manager.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/auth/user_manager.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/core/leaderboard.cpp" "CMakeFiles/TypeDrift.dir/src/core/leaderboard.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/core/leaderboard.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/core/session_logger.cpp" "CMakeFiles/TypeDrift.dir/src/core/session_logger.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/core/session_logger.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/game_modes/SpeedTest.cpp" "CMakeFiles/TypeDrift.dir/src/game_modes/SpeedTest.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/game_modes/SpeedTest.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/game_modes/TerminalSetup.cpp" "CMakeFiles/TypeDrift.dir/src/game_modes/TerminalSetup.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/game_modes/TerminalSetup.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/game_modes/TimeTest.cpp" "CMakeFiles/TypeDrift.dir/src/game_modes/TimeTest.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/game_modes/TimeTest.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/game_modes/falling_words.cpp" "CMakeFiles/TypeDrift.dir/src/game_modes/falling_words.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/game_modes/falling_words.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/game_modes/vowel_game.cpp" "CMakeFiles/TypeDrift.dir/src/game_modes/vowel_game.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/game_modes/vowel_game.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/main.cpp" "CMakeFiles/TypeDrift.dir/src/main.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/main.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/utils/file_helper.cpp" "CMakeFiles/TypeDrift.dir/src/utils/file_helper.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/utils/file_helper.cpp.o.d"
+  "/home/luimas834/Desktop/TypeDrift/code/src/utils/user.cpp" "CMakeFiles/TypeDrift.dir/src/utils/user.cpp.o" "gcc" "CMakeFiles/TypeDrift.dir/src/utils/user.cpp.o.d"
+  "" "TypeDrift" "gcc" "CMakeFiles/TypeDrift.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ code/build/CMakeFiles/TypeDrift.dir/src/game_modes/falling_words.cpp.o.d
@@ -0,0 +1,7 @@
+CMakeFiles/TypeDrift.dir/src/game_modes/falling_words.cpp.o: \
+ /home/luimas834/Desktop/TypeDrift/code/src/game_modes/falling_words.cpp \
+ /usr/include/stdc-predef.h \
+ /home/luimas834/Desktop/TypeDrift/code/src/game_modes/fw_common.h \
+ /home/luimas834/Desktop/TypeDrift/code/src/game_modes/fw_animation.h \
+ /home/luimas834/Desktop/TypeDrift/code/src/game_modes/fw_database.h \
+ /home/luimas834/Desktop/TypeDrift/code/src/game_modes/fw_score.h
